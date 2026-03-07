7848054cbcfd6b9462df974c957010197f910bfd
Mazen2024 <154472378+Mazen2024@users.noreply.github.com>
2026-03-07 11:02:08 +0200

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -33,11 +33,17 @@ export default async function proxy(request: NextRequest) {
   const pathnameIsMissingLocale = i18n.locales.every(
     (locale) => !pathname.startsWith(`/${locale}`),
   );
-  // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-    return NextResponse.redirect(new URL(`/${locale}${pathname}`, request.url));
+
+      // لو المستخدم داخل على /
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL("/en", request.url));
   }
+  
+  // // Redirect if there is no locale
+  // if (pathnameIsMissingLocale) {
+  //   const locale = getLocale(request);
+  //   return NextResponse.redirect(new URL(`/${locale}${pathname}`, request.url));
+  // }
 
   return NextResponse.next({
     request: {
